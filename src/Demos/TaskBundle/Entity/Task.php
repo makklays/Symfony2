eed9459875edfba8e4d82e668189246d68ba4dda
Alexander Kuziv <makklays@gmail.com>
2018-02-23 18:29:42 +0300

    Create Task.php

---
+++ src/Demos/TaskBundle/Entity/Task.php
@@ -0,0 +1,283 @@
+<?php
+namespace Demos\TaskBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="task")
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $task;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $priority;
+
+    /**
+     * @ORM\Column(type="text", nullable=true, options="{default:NULL}")
+     */
+    protected $attachment;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $isCompleted;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_date;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set task
+     *
+     * @param string $task
+     * @return Task
+     */
+    public function setTask($task)
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+
+    /**
+     * Get task
+     *
+     * @return string 
+     */
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    /**
+     * Set created_date
+     *
+     * @param \DateTime $createdDate
+     * @return Task
+     */
+    public function setCreatedDate($createdDate)
+    {
+        $this->created_date = $createdDate;
+
+        return $this;
+    }
+
+    /**
+     * Get created_date
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedDate()
+    {
+        return $this->created_date;
+    }
+
+    /**
+     * Set updated_date
+     *
+     * @param \DateTime $updatedDate
+     * @return Task
+     */
+    public function setUpdatedDate($updatedDate)
+    {
+        $this->updated_date = $updatedDate;
+
+        return $this;
+    }
+
+    /**
+     * Get updated_date
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedDate()
+    {
+        return $this->updated_date;
+    }
+
+    
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Task
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Task
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+
+
+    /**
+     * Set priority
+     *
+     * @param string $priority
+     * @return Task
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return string 
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * Set isCompleted
+     *
+     * @param integer $isCompleted
+     * @return Task
+     */
+    public function setIsCompleted($isCompleted)
+    {
+        $this->isCompleted = $isCompleted;
+
+        return $this;
+    }
+
+    /**
+     * Get isCompleted
+     *
+     * @return integer 
+     */
+    public function getIsCompleted()
+    {
+        return $this->isCompleted;
+    }
+
+    /**
+     * Set attachment
+     *
+     * @param string $attachment
+     * @return Task
+     */
+    public function setAttachment($attachment)
+    {
+        $this->attachment = $attachment;
+
+        return $this;
+    }
+
+    /**
+     * Get attachment
+     *
+     * @return string 
+     */
+    public function getAttachment()
+    {
+        return $this->attachment;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     * @return Task
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
